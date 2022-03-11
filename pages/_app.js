b2bad1e7fe725b11326597590966f9ba25209a85
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-11 00:09:08 -0300

    Add app, document and index on application

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+import { ModalContextProvider } from '../utils/modalContext'
+import { CityContextProvider } from '../utils/citiesContext'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ModalContextProvider>
+      <CityContextProvider>
+        <Component {...pageProps} />
+      </CityContextProvider>
+    </ModalContextProvider>
+  )
 }
 
 export default MyApp
