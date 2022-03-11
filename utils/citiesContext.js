6299adbf78808b60901cc8073d0eba95ce884a59
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-11 00:08:00 -0300

    Context to cities

---
+++ utils/citiesContext.js
@@ -0,0 +1,43 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect
+} from 'react'
+
+export const CityContext = createContext({})
+
+const INITIAL_STATE_ALL_CITIES = [
+  { id: 1, name: 'Americana' },
+  { id: 2, name: 'Campinas' },
+  { id: 3, name: 'Hortolândia' },
+  { id: 4, name: 'Indaiatuba' },
+  { id: 5, name: 'Jaguariúna' },
+  { id: 6, name: 'Jundiaí' }
+]
+
+export const CityContextProvider = ({ children }) => {
+  const [allCities, setAllCities] = useState(INITIAL_STATE_ALL_CITIES)
+  
+  const [citySelected, setCitySelected] = useState(
+    INITIAL_STATE_ALL_CITIES.length === 1
+      ? INITIAL_STATE_ALL_CITIES[0]
+      : null
+  )
+
+  const state = {
+    allCities,
+    citySelected,
+    setCitySelected
+  }
+
+  return (
+    <CityContext.Provider value={state}>
+      {children}
+    </CityContext.Provider>
+  )
+}
+
+export const useCities = () => {
+  return useContext(CityContext)
+}
