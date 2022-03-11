74146ac286b472afb8a68c5e16cc1d522c620b10
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-11 00:11:27 -0300

    Add fake data to consume in the feed

---
+++ utils/datas.js
@@ -0,0 +1,32 @@
+export const coupons = [
+  {
+    type: "pro",
+    companyName: "Smash! Burguer House",
+    title: "Hamburguer com 10% OFF",
+    description: "Desconto de 25% em qualquer compra realizada acima de $50 em nosso estabelicimento. Válido até 22 de Fevereiro de 2022.",
+    color: '#F72F0E',
+    textColor: '#FFFFFF',
+    logo: '/images/logo_example2.png',
+  },
+  {
+    type: "premium",
+    companyName: "Smash! Burguer House",
+    title: "Hamburguer com 10% OFF",
+    description: "Desconto de 25% em qualquer compra realizada acima de $50 em nosso estabelicimento. Válido até 22 de Fevereiro de 2022.",
+    color: '#F72F0E',
+    textColor: '#FFFFFF',
+    logo: '/images/logo_example2.png',
+  },
+  {
+    type: "free",
+    companyName: "Smash! Burguer House",
+    title: "Hamburguer com 10% OFF",
+    description: "Desconto de 25% em qualquer compra realizada acima de $50 em nosso estabelicimento. Válido até 22 de Fevereiro de 2022."
+  },
+  {
+    type: "free",
+    companyName: "Smash! Burguer House",
+    title: "Hamburguer com 10% OFF",
+    description: "Desconto de 25% em qualquer compra realizada acima de $50 em nosso estabelicimento. Válido até 22 de Fevereiro de 2022."
+  }
+]
