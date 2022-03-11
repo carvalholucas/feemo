786824c13ac42b03ea2eb9906a1b87b484dc1b0b
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-11 00:09:31 -0300

    Login page

---
+++ pages/login.js
@@ -0,0 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Container } from '../components/Layout'
+import { Form } from '../components/Form'
+import Field from '../components/Field'
+import Button from '../components/Button'
+
+const Login = () => {
+  return (
+    <Container>
+      <Form>
+        <Image
+          src="/images/logo_dark.png"
+          width={162}
+          height={50}
+        />
+        <Field
+          label="Email"
+          placeholder="Informe seu email"
+        />
+
+        <Field
+          label="Senha"
+          type="password"
+          placeholder="Informe sua senha"
+        />
+
+        <Link href="/reset">
+          <a className="link link-forget">Esqueci senha?</a>
+        </Link>
+
+        <Button
+          label="Entrar"
+          color="secondary"
+          style={{ marginTop: '1rem' }}
+        />
+
+        <Link href="/register">
+          <a className="link">Sem acesso ainda? <span>Criar conta</span></a>
+        </Link>
+      </Form>
+    </Container>
+  )
+}
+
+export default Login
