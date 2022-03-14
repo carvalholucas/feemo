6029da009a0f4c2e79dc07cf6e04a7fcb045f994
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-14 17:41:08 -0300

    Change data

--- utils/datas.js
+++ utils/datas.js
@@ -3,7 +3,7 @@ export const coupons = [
     type: "premium",
     companyName: "Smash! Burguer House",
     title: "Hamburguer com 10% OFF",
-    description: "Desconto de 25% em qualquer compra realizada acima de $50 em nosso estabelicimento. Válido até 22 de Fevereiro de 2022.",
+    description: "Desconto de 25% em qualquer compra realizada acima de $50 em nosso...",
     color: '247, 47, 47',
     logo: '/images/logo_example2.png',
   },
