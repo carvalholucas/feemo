ddbaf182b232d999c3cb6dc79b733e8df8694186
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-14 15:28:06 -0300

    Remove filter button from feed

--- components/Feed/index.js
+++ components/Feed/index.js
@@ -1,23 +1,8 @@
-import { IoFilter as FilterIcon } from "react-icons/io5"
-
-import {
-  Container,
-  Title,
-  Icon
-} from './styles'
+import { Container } from './styles'
 
 const Feed = ({ children }) => {
   return (
     <Container>
-      <Title>
-        <Icon>
-          <FilterIcon
-            color="#362076"
-            size="1rem"
-          />
-          <span>Filtrar</span>
-        </Icon>
-      </Title>
       {children}
     </Container>
   )
--- components/Feed/styles.js
+++ components/Feed/styles.js
@@ -3,22 +3,3 @@ import styled from 'styled-components'
 export const Container = styled.section`
   padding: 1rem;
 `
-
-export const Title = styled.div`
-  align-items: center;
-  color: #362076;
-  display: flex;
-  font-size: .8rem;
-  justify-content: flex-end;
-
-  span {
-    margin-left: .5rem;
-  }
-`
-
-export const Icon = styled.div`
-  background: #D8D5E2;
-  border-radius: 5px;
-  display: flex;
-  padding: .5rem;
-`
