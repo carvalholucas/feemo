99196f6050d1cdb29b389a62b2361cf8b1f9bb00
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-14 17:24:48 -0300

    Added new font family from google

--- pages/_document.js
+++ pages/_document.js
@@ -36,7 +36,11 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <div id="modal-root" />
