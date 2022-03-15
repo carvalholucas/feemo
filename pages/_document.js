8b990bc3924c7d745ab2ed7618bf75074b8136cd
Lucas Carvalho <carvalho.lucassm@hotmail.com>
2022-03-15 00:48:59 -0300

    Added theme-color

--- pages/_document.js
+++ pages/_document.js
@@ -38,8 +38,9 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap" rel="stylesheet" />
+          <meta name="theme-color" content="#351b78"/>
         </Head>
         <body>
           <Main />
